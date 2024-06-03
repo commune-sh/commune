e345cce5a250cb6910cf84362a8ee168c428c39e
hummingbard <mail@hummingbard.com>
2024-06-03 13:37:47 +0600

    drag/drop mechanics

--- src/lib/switcher/item.svelte
+++ src/lib/switcher/item.svelte
@@ -1,28 +1,98 @@
 <script>
 import { draggable } from '@neodrag/svelte';
 
-let { item, move } = $props();
+let { item, activate, active, moving, moved, end } = $props();
 
 
 const initial = $derived(item.name.charAt(0).toUpperCase())
 
-function mo() {
-    move(item.id)
+// is this item being dragged?
+let dragging = $state(false);
+
+// item started to be dragged
+function dragstart(e) {
+    e.dataTransfer.setData('text/plain', item.id)
+    activate(item.id);
 }
 
-let options = {
-    axis: 'y',
-    bounds: 'parent',
-  };
+// drag ended
+function dragend() {
+    dragging = false;
+    end()
+}
+
+// dropzone state
+let dropzone = $derived(moved == item?.id && active != item?.id);
+
+let clientY = $state(0);
+
+// top zone
+let top = $derived(clientY < 108);
+
+// is another item being dragged over this one?
+function dragover(e) {
+    e.preventDefault();
+    moving(item.id);
+    clientY = e.clientY;
+}
+
+function drag(e) {
+    dragging = true;
+}
+
+// dropped
+function drop(e) {
+    e.preventDefault();
+    console.log(`item ${active} dropped over item ${moved}`)
+    clientY = 0;
+}
+
 </script>
 
-<div class="grid place-items-center mb-[10px]" use:draggable={options}>
-    <div class="bg-shade-5 w-[46px] h-[46px] rounded-[50%] grid
+<div class="grid relative place-items-center mb-[10px]">
+    <div class:dragging={dragging} 
+        class="item bg-shade-5 w-[46px] h-[46px] rounded-[50%] grid
         hover:rounded-[14px]
         transition-transform duration-200
-        place-items-center cursor-pointer hover:bg-shade-7">
-        <div class="font-semibold">
+        place-items-center cursor-pointer hover:bg-shade-7"
+        draggable="true"
+        ondrag={drag}
+        ondragend={dragend}
+        ondragleave={dragend}
+        ondragover={dragover}
+        ondrop={drop}
+        ondragstart={dragstart}>
+        <div class="initial font-semibold">
         {initial}
         </div>
     </div>
+
+    {#if dropzone && !top}
+        <div class="absolute border-2 border-red bottom-[-6px] left-0 right-0
+            mx-[6px] rounded-[3px]">
+        </div>
+    {/if}
+    {#if dropzone && top}
+        <div class="absolute border-2 border-red top-[-2px] left-0 right-0
+            mx-[6px] rounded-[3px]">
+        </div>
+    {/if}
+
 </div>
+
+<style>
+.item {
+    transform: translate(0, 0);
+}
+.dragging {
+    opacity: 0.2;
+}
+
+.dragging .initial {
+    opacity: 0;
+}
+
+.dragover {
+    border-bottom: 2px solid red;
+}
+</style>
--- src/lib/switcher/items.svelte
+++ src/lib/switcher/items.svelte
@@ -2,9 +2,12 @@
 import Item from './item.svelte'
 
 let items = $state([
+    { id: 0, name: 'Programming' },
     { id: 1, name: 'Music' },
     { id: 2, name: 'Books' },
     { id: 3, name: 'News' },
+    { id: 4, name: 'Videos' },
+    { id: 5, name: 'Wiki' },
 ])
 
 function move(id) {
@@ -16,10 +19,34 @@ function move(id) {
     }
 }
 
+// item being dragged over
+let moved = $state(null);
+
+// item started to move
+function moving(id) {
+    moved = id;
+}
+
+// kill move state
+function end() {
+    moved = null;
+}
+
+let active = $state(null);
+
+function activate(id) {
+    active = id;
+}
+
 </script>
 
 <div class="h-100">
-    {#each items as item}
-        <Item move={move} {item} />
+    {#each items as item(item.id)}
+        <Item {item} {moved} {active}
+        move={move} 
+        moving={moving} 
+        activate={activate} 
+        end={end} />
     {/each}
+
 </div>
