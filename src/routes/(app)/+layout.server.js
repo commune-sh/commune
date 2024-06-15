ab75458e8a718b7985c0d242d4bcd894ff53cb3a
hummingbard <mail@hummingbard.com>
2024-06-15 22:44:50 +0600

    catch on public server fetch failure

--- src/routes/(app)/+layout.server.js
+++ src/routes/(app)/+layout.server.js
@@ -12,13 +12,23 @@ export async function load( { fetch, params, url, cookies, request } ) {
   // fetch public server capabilities
 
   if(PUBLIC_SERVER != "") {
-    let url = `${PUBLIC_SERVER}/capabilities`;
-    const res = await fetch( url );
-    const resp = await res.json();
-    if(resp?.capabilities) {
-      data.public_server_exists = true;
-      data.capabilities = resp?.capabilities;
+
+    try {
+
+      let url = `${PUBLIC_SERVER}/capabilities`;
+      const res = await fetch( url );
+      const resp = await res.json();
+      if(resp?.capabilities) {
+        data.public_server_exists = true;
+        data.capabilities = resp?.capabilities;
+      }
+
+    } catch (error) {
+
+      data["public_server_unreachable"] = true;
+
     }
+
   }
 
   return data;
