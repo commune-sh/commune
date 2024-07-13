a00e705b317a294164d17b5b3d6ae2c4151800ab
hummingbard <mail@hummingbard.com>
2024-07-13 23:38:34 +0600

    thread container resize

--- src/lib/thread/thread.svelte
+++ src/lib/thread/thread.svelte
@@ -1,9 +1,96 @@
 <script>
+import { getSetting, updateSetting } from '$lib/utils/localstorage.js';
 
-let width = $state(400);
+
+let saved_width = $derived.by(() => {
+    return getSetting('thread_width');
+});
+
+let width = $state(saved_width || 232);
+
+let resizing = $state(false);
+
+let startX = $state(0);
+
+const start = (e) => {
+    resizing = true;
+    startX = e.clientX;
+    window.addEventListener('mousemove', resize);
+    window.addEventListener('mouseup', done);
+};
+
+const resize = (e) => {
+    if(resizing) {
+        const deltaX = e.clientX - startX;
+        if(deltaX > 0 && width <= 400) return;
+        if(deltaX < 0 && width >= 600) return;
+        width += -(deltaX)
+        startX = e.clientX;
+    }
+};
+
+const done = () => {
+    resizing = false;
+    updateSetting('thread_width', width);
+    window.removeEventListener('mousemove', resize);
+    window.removeEventListener('mouseup', done);
+};
+
+$effect(() => {
+    if(resizing) {
+        document.body.classList.add('resize-cursor');
+    } else {
+        document.body.classList.remove('resize-cursor');
+    }
+})
+
+
 </script>
 
-<div class="thread grid border-solid border-l border-sidebar-border"
-    style="width: {width}px">
-    thread
+<div class="thread-container relative grid">
+    <div class="thread grid border-solid border-l border-sidebar-border"
+        style="width: {width}px">
+    </div>
+
+    <div class="dragger absolute" 
+        class:resizing={resizing}
+        onmousedown={start}>
+        <div class="holder" >
+        </div>
+    </div>
+
 </div>
+
+<style>
+
+.dragger {
+    width: 11px;
+    left: -6px;
+    top: 0;
+    bottom: 0;
+    cursor: col-resize;
+    border-radius: 1px;
+    opacity: 0;
+    transition: opacity 0.2s;
+    z-index: 10;
+}
+
+.resizing {
+    opacity: 1;
+}
+
+.dragger:hover {
+    opacity: 1;
+}
+
+.holder {
+    height: 100%;
+    width: 1px;
+    background: var(--dragger);
+    margin-left: 5px;
+    z-index: 12;
+}
+</style>
+
+
+
