bb4cee0af24c073000f375ff4a9b07417c0ecb09
hummingbard <mail@hummingbard.com>
2024-08-01 10:57:41 +0600

    render mxc custom emoji

--- src/lib/room/common/m.reaction.svelte
+++ src/lib/room/common/m.reaction.svelte
@@ -1,4 +1,8 @@
 <script>
+import { 
+    thumbnailURL,
+} from '$lib/utils/matrix'
+
 import { createStore } from '$lib/store/store.svelte.js'
 const store = createStore()
 
@@ -6,12 +10,25 @@ let {
     key,
     events,
 } = $props();
+
+const is_custom = $derived.by(() => {
+    return key.startsWith('mxc://')
+})
+
+const reaction = $derived.by(() => {
+    return is_custom ? thumbnailURL(key, 32, 32, 'crop') : key
+})
+
 </script>
 
 <div class="grid grid-cols-[auto_auto] px-1 place-items-center
-    bg-shade-3 text-light mr-1 rounded-[4px]">
+    cursor-pointer bg-shade-3 text-light mr-1 rounded-[4px]">
     <div class="emoji">
-        {key}
+        {#if is_custom}
+            <img src={reaction} width="16" height="16" alt={key} />
+        {:else}
+            {key}
+        {/if}
     </div>
     <div class="text-xs ml-1">
         {events?.length}
