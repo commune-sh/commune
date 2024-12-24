18f9dbea3690cb1422356cd630f9bd5ca7f302eb
hummingbard <mail@hummingbard.com>
2024-12-24 18:55:16 +0600

    tweak landing meta

--- src/lib/landing/landing.svelte
+++ src/lib/landing/landing.svelte
@@ -1,31 +1,49 @@
 <script>
+import { logo } from '$lib/assets/logo.js';
 </script>
 
 <div class="landing bg-view">
+
     <div class="mt-mid flex flex-col items-center">
 
-            <div class="name">
-                commune
-            </div>
+        <div class="logo ">
+            {@html logo}
+        </div>
 
-            <div class="meta pt-6">
-                matrix-powered public spaces
-            </div>
+        <div class="name pt-6">
+            commune
+        </div>
 
-            <div class="pt-16">
-                <button class="">Get Started</button>
-            </div>
+        <div class="meta mt-4">
+            Matrix spaces, accessible from the open web
+        </div>
+
+        <div class="pt-16">
+            <button class="">Build your commune</button>
+        </div>
 
     </div>
+
 </div>
 
 <style>
+
+.logo {
+    display: none;
+    background-color: var(--logo-background);
+    fill: var(--logo-fill);
+    border-radius: 14px;
+    width: 46px;
+    height: 46px;
+}
 
 button {
     border-radius: 500px;
     padding: 0.75rem 1.5rem;
     background-color: var(--text);
     color: var(--background);
+    font-size: 1rem;
+    font-weight: 600;
 }
 
 .landing {
@@ -34,35 +52,45 @@ button {
     height: 100vh;
 }
 
+.mt-mid {
+    margin-top: calc(50vh / 2);
+}
+
 .name {
-    font-size: 4rem;
-    font-weight: 700;
+    font-size: 3.8rem;
+    font-weight: 900;
+    text-transform: uppercase;
 }
 
 .meta {
-    font-size: 1.8rem;
+    font-size: 1.6rem;
     font-weight: 600;
+    text-align: center;
+    line-height: 1.5;
+    padding: 0 2rem;
 }
 
 @media (max-width: 1024px) {
     .name {
-        font-size: 3rem;
+        font-size: 2.8rem;
     }
-
     .meta {
-        font-size: 1.6rem;
+        font-size: 1.4rem;
     }
 }
 
 @media (max-width: 768px) {
+    .logo {
+        display: block;
+    }
     .landing {
         padding-right: 0;
     }
     .name {
-        font-size: 2.5rem;
+        font-size: 2.4rem;
     }
     .meta {
-        font-size: 1.4rem;
+        font-size: 1.2rem;
     }
 }
 
