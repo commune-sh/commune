6dc3a56999e9f53d280467f3085700b6f3424e01
hummingbard <mail@hummingbard.com>
2025-03-24 21:52:25 +0600

    register route

---
+++ src/routes/(auth)/register/+page.server.ts
@@ -0,0 +1,37 @@
+import { PUBLIC_BASE_URL } from '$env/static/public';
+
+import type { PageServerLoad } from './$types';
+import { redirect } from "@sveltejs/kit";
+import { generateDeviceId, generatePKCEParams } from '$lib/utils/oidc'
+
+export const load: PageServerLoad = async ({ cookies }) => {
+
+    let redirect_url = `${PUBLIC_BASE_URL}/oidc/callback`;
+
+    const oidc_client_id = cookies.get('oidc_client_id');
+
+    const authorization_endpoint = cookies.get('oidc_authorization_endpoint');
+
+    let scope = `urn:matrix:org.matrix.msc2967.client:api:*`;
+
+    let pkce = await generatePKCEParams();
+
+    cookies.set('oidc_code_verifier', pkce.code_verifier, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 365,
+        secure: true,
+        sameSite: 'strict',
+        path: '/'
+    });
+
+
+    let url = `${authorization_endpoint}?client_id=${oidc_client_id}&redirect_uri=${redirect_url}&response_type=code&response_mode=query&scope=${scope}&state=${pkce.state}&code_challenge=${pkce.code_challenge}&code_challenge_method=S256&prompt=create`;
+
+    let encoded_url = encodeURI(url);
+
+    redirect(302, encoded_url);
+
+    if (!oidc_client_id) {
+        redirect(302, '/');
+    }
+};
