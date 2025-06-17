3750dd3ba5e8381d999f194316257768559b688d
hummingbard <mail@hummingbard.com>
2025-06-17 16:19:11 +0600

    ignore csrf in dev

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV !== 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
+  kit: {
+    adapter: adapter(),
+    csrf: { checkOrigin: !dev }
+  },
   preprocess: vitePreprocess()
 };
 
