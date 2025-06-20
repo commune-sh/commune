db81441775471c9106f3578de6a59c443f7fb58f
hummingbard <mail@hummingbard.com>
2025-06-20 22:20:45 +0600

    x

--- src/lib/store/auth.svelte.ts
+++ src/lib/store/auth.svelte.ts
@@ -22,179 +22,179 @@ let authenticated = $state(false);
 let login_flows = $state(null);
 
 if(browser) {
-  let access_token = getCookie('mx_access_token')
-  if(access_token == null) {
-    ready = true
-  }
+    let access_token = getCookie('mx_access_token')
+    if(access_token == null) {
+        ready = true
+    }
 }
 
 export function createAuthStore() {
 
-  async function setup(opts) {
+    async function setup(opts) {
 
-    if(opts?.authenticated && opts?.access_token && opts?.user_id) {
-      console.log("Setting up Auth store. User is authenticated.")
+        if(opts?.authenticated && opts?.access_token && opts?.user_id) {
+            console.log("Setting up Auth store. User is authenticated.")
 
-      access_token_valid = true;
-      access_token_checked = true;
+            access_token_valid = true;
+            access_token_checked = true;
 
-      credentials = {
-        access_token: opts.access_token,
-        user_id: opts.user_id,
-        device_id: opts.device_id,
-      }
-      authenticated = true;
+            credentials = {
+                access_token: opts.access_token,
+                user_id: opts.user_id,
+                device_id: opts.device_id,
+            }
+            authenticated = true;
 
-      ready = true
-      return
-
-
-    } else {
-      console.log("Setting up Auth store.")
-    }
+            ready = true
+            return
 
 
-    const access_token = getCookie('mx_access_token')
-    const user_id = getCookie('mx_user_id')
-    const device_id = getCookie('mx_device_id')
-
-    if(!access_token || !user_id || !device_id) {
-      console.log("No credentials found in local storage.")
-      ready = true
-      return
-    }
-
-    if(access_token && user_id) {
-
-      try {
-        const response = await whoami(access_token);
-        if(response.errcode == "M_UNKNOWN_TOKEN") {
-          console.log("Access token is invalid.")
-          //purge();
-          access_token_checked = true;
-          ready = true
-          return
+        } else {
+            console.log("Setting up Auth store.")
         }
-        if(response.user_id == user_id && response.device_id == device_id) {
-          console.log("Access token is valid.")
-          access_token_valid = true;
-          access_token_checked = true;
-          authenticated = true;
-          ready = true
+
+
+        const access_token = getCookie('mx_access_token')
+        const user_id = getCookie('mx_user_id')
+        const device_id = getCookie('mx_device_id')
+
+        if(!access_token || !user_id || !device_id) {
+            console.log("No credentials found in local storage.")
+            ready = true
+            return
         }
-        //authenticated = true;
-      } catch (error) {
-        console.log(error)
-        //purge();
-      }
+
+        if(access_token && user_id) {
+
+            try {
+                const response = await whoami(access_token);
+                if(response.errcode == "M_UNKNOWN_TOKEN") {
+                    console.log("Access token is invalid.")
+                    //purge();
+                    access_token_checked = true;
+                    ready = true
+                    return
+                }
+                if(response.user_id == user_id && response.device_id == device_id) {
+                    console.log("Access token is valid.")
+                    access_token_valid = true;
+                    access_token_checked = true;
+                    authenticated = true;
+                    ready = true
+                }
+                //authenticated = true;
+            } catch (error) {
+                console.log(error)
+                //purge();
+            }
+
+        }
+
+        if(access_token && user_id && device_id) {
+            credentials = {
+                access_token: access_token,
+                user_id: user_id,
+                device_id: device_id,
+            }
+            console.log("Credentials loaded from local storage.", $state.snapshot(credentials))
+        } else {
+            console.log("No credentials found in local storage.")
+        }
+
+        ready = true
+    }
+
+    function purge() {
+        console.log("Purging credentials.")
+        removeCookie('mx_access_token')
+        removeCookie('mx_user_id')
+        removeCookie('mx_device_id')
+        credentials = null
+        authenticated = false;
+    }
+
+    function validateAccessToken() {
+        access_token_checked = true;
+    }
+
+    function updateLoginFlows(flows) {
+        console.log("Storing login flows: ", flows)
+        login_flows = flows
+    }
+
+    function saveSession(opts) {
+        if(!opts?.access_token || 
+            !opts?.user_id || 
+            !opts?.device_id) {
+            return 
+        }
+        storeCookies({
+            mx_access_token: opts.access_token,
+            mx_user_id: opts.user_id,
+            mx_device_id: opts.device_id,
+            mx_home_server: opts.home_server,
+        })
+        credentials = {
+            access_token: opts.access_token,
+            user_id: opts.user_id,
+            device_id: opts.device_id,
+        }
+        authenticated = true;
+        access_token_valid = true;
+        access_token_checked = true;
+        ready = true
+    }
+
+    function saveGuestSession(opts) {
+        if(!opts?.access_token || 
+            !opts?.user_id || 
+            !opts?.device_id) {
+            return 
+        }
+        storeCookies({
+            mx_guest_access_token: opts.access_token,
+            mx_guest_user_id: opts.user_id,
+            mx_guest_device_id: opts.device_id,
+            mx_guest_home_server: opts.home_server,
+        })
+        guest_credentials = {
+            access_token: opts.access_token,
+            user_id: opts.user_id,
+            device_id: opts.device_id,
+        }
 
     }
 
-    if(access_token && user_id && device_id) {
-      credentials = {
-        access_token: access_token,
-        user_id: user_id,
-        device_id: device_id,
-      }
-      console.log("Credentials loaded from local storage.", $state.snapshot(credentials))
-    } else {
-      console.log("No credentials found in local storage.")
-    }
+    return {
+        get ready() {
+            return ready;
+        },
 
-    ready = true
-  }
+        get authenticated() {
+            return authenticated;
+        },
 
-  function purge() {
-    console.log("Purging credentials.")
-    removeCookie('mx_access_token')
-    removeCookie('mx_user_id')
-    removeCookie('mx_device_id')
-    credentials = null
-    authenticated = false;
-  }
+        get credentials() {
+            return credentials;
+        },
 
-  function validateAccessToken() {
-    access_token_checked = true;
-  }
+        get access_token_valid() {
+            return access_token_valid;
+        },
 
-  function updateLoginFlows(flows) {
-    console.log("Storing login flows: ", flows)
-    login_flows = flows
-  }
+        get access_token_checked() {
+            return access_token_checked;
+        },
 
-  function saveSession(opts) {
-    if(!opts?.access_token || 
-      !opts?.user_id || 
-      !opts?.device_id) {
-      return 
-    }
-    storeCookies({
-      mx_access_token: opts.access_token,
-      mx_user_id: opts.user_id,
-      mx_device_id: opts.device_id,
-      mx_home_server: opts.home_server,
-    })
-    credentials = {
-      access_token: opts.access_token,
-      user_id: opts.user_id,
-      device_id: opts.device_id,
-    }
-    authenticated = true;
-    access_token_valid = true;
-    access_token_checked = true;
-    ready = true
-  }
+        get login_flows() {
+            return login_flows;
+        },
 
-  function saveGuestSession(opts) {
-    if(!opts?.access_token || 
-      !opts?.user_id || 
-      !opts?.device_id) {
-      return 
-    }
-    storeCookies({
-      mx_guest_access_token: opts.access_token,
-      mx_guest_user_id: opts.user_id,
-      mx_guest_device_id: opts.device_id,
-      mx_guest_home_server: opts.home_server,
-    })
-    guest_credentials = {
-      access_token: opts.access_token,
-      user_id: opts.user_id,
-      device_id: opts.device_id,
-    }
-
-  }
-
-  return {
-    get ready() {
-      return ready;
-    },
-
-    get authenticated() {
-      return authenticated;
-    },
-
-    get credentials() {
-      return credentials;
-    },
-
-    get access_token_valid() {
-      return access_token_valid;
-    },
-
-    get access_token_checked() {
-      return access_token_checked;
-    },
-
-    get login_flows() {
-      return login_flows;
-    },
-
-    setup,
-    purge,
-    validateAccessToken,
-    updateLoginFlows,
-    saveSession,
-    saveGuestSession,
-  };
+        setup,
+        purge,
+        validateAccessToken,
+        updateLoginFlows,
+        saveSession,
+        saveGuestSession,
+    };
 }
